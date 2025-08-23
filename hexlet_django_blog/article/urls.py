efdc8b1140a7d4b3ace45154a8d7327cd9f07bd8
oleg_dixon <olegdixon5973@gmail.com>
2025-08-23 13:25:55 +0500

    Other fixes

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormCreateView,
+    ArticleFormEditView,
+    ArticleFormDeleteView,
+)
 
 urlpatterns = [
-    path("", IndexView.as_view()),
+    path("", IndexView.as_view(), name='articles_index'),
+    path('<int:id>/update/', ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:id>/', ArticleView.as_view(), name='articles_show'),
-    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+    # path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
+    
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect
+)
+from django.contrib import messages
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.forms import ArticleForm
 
 
 class IndexView(View):
@@ -28,14 +34,75 @@ class ArticleView(View):
         )
     
 
-class ArticleCommentsView(View):
+class ArticleFormCreateView(View):
     def get(self, request, *args, **kwargs):
-        comment = get_object_or_404(
-            Сomment, id=kwargs['id'], article_id=kwargs['article_id']
-        )
+        form = ArticleForm()
         return render(
-            'comment/show.html',
-            context={
-                'comment': comment
-            },
+            request,
+            'articles/create.html',
+            {'form': form}
         )
+    
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/articles')
+        return render(
+            request,
+            'articles/create.html',
+            {'form': form}
+        )
+
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': article_id},
+        )
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+        messages.success(request, 'Статья успешно отредактирована.')
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': article_id},
+        )
+
+
+class ArticleFormDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        return render(
+            request,
+            'articles/delete.html',
+            {'article': article},
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        messages.success(request, 'Статья успешно удалена.')
+        return redirect('articles_index')
+
+
+# class ArticleCommentsView(View):
+#     def post(self, request, *args, **kwargs):
+#         form = ArticleCommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.content = check_for_spam(form.data['content'])
